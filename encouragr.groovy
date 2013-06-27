91a811326850b010565686e311a0b6945acd7cd2
lordfoom <lordfoom@gmail.com>
2013-06-27 22:44:02 +0200

    Some cleanup

--- encouragr.groovy
+++ encouragr.groovy
@@ -6,9 +6,9 @@ import java.util.Random
 
 font = new Font("SansSerif", Font.BOLD, 46)
 
-def happy_thoughts = new File("happy_thoughts.groovy").readLines()
-Random rand = new Random()
-def message = happy_thoughts[rand.nextInt(happy_thoughts.size)]
+happy_thoughts = new File("happy_thoughts.groovy").readLines()
+rand = new Random()
+message = happy_thoughts[rand.nextInt(happy_thoughts.size)]
 
 myFrame = new SwingBuilder().edt {
   frame(title:'You rock!', size:[140,100], show: true, pack:true, locationRelativeTo: null, defaultCloseOperation:JFrame.EXIT_ON_CLOSE) {
