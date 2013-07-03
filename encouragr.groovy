188d9ccb8afa8b097b69a7958bb251d72af268ed
Tim Spring <lordfoom@FOOMTOPIA.(none)>
2013-07-03 17:49:14 +0200

    Change button text; let OS determine starting location

--- encouragr.groovy
+++ encouragr.groovy
@@ -1,3 +1,4 @@
+#!/usr/bin/env groovy
 import groovy.swing.SwingBuilder
 import java.awt.BorderLayout as BL
 import java.awt.Font
@@ -11,10 +12,10 @@ rand = new Random()
 message = happy_thoughts[rand.nextInt(happy_thoughts.size)]
 
 myFrame = new SwingBuilder().edt {
-  frame(title:'You rock!', size:[140,100], show: true, pack:true, locationRelativeTo: null, defaultCloseOperation:JFrame.EXIT_ON_CLOSE) {
+  frame(title:'You rock!', size:[140,100], show: true, pack:true, locationByPlatform: true, defaultCloseOperation:JFrame.EXIT_ON_CLOSE) {
     borderLayout()
     textlabel = label(text:message, font: font, constraints: BL.CENTER)
-    button(text:' Yes! ',
+    button(text:" It's True! ",
          actionPerformed: {myFrame.dispose()},
          constraints:BL.SOUTH)
   }
