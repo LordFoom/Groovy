dd6cbff29676f83b6ebe3fffb45789f817c79612
Tim Spring <lordfoom@FOOMTOPIA.(none)>
2013-09-12 11:00:51 +0200

    Added optional configuration for button text

--- encouragr.groovy
+++ encouragr.groovy
@@ -13,12 +13,20 @@ scriptDir = new File(getClass().protectionDomain.codeSource.location.path).paren
 happy_thoughts = new File(scriptDir+"/happy_thoughts.groovy").readLines()
 rand = new Random()
 message = happy_thoughts[rand.nextInt(happy_thoughts.size)]
+button_text = "It's true!"
+//do we have button text, ie text that comes after a ':'?
+message_parts = message.split(":")
+if(message_parts.size() == 2)
+{
+    message = message_parts[0]
+    button_text = message_parts[1]
+}
 
 myFrame = new SwingBuilder().edt {
   frame(title:'You rock!', size:[140,100], show: true, pack:true, locationByPlatform: true, defaultCloseOperation:JFrame.EXIT_ON_CLOSE) {
     borderLayout()
     textlabel = label(text:" "+message+" ", font: font, constraints: BL.CENTER)
-    button(text:" It's True! ",
+    button(text:button_text,
          actionPerformed: {myFrame.dispose()},
          constraints:BL.SOUTH)
   }
--- happy_thoughts.groovy
+++ happy_thoughts.groovy
@@ -1,3 +1,5 @@
-You SO Cool!
+You SO Cool!:I really am
 You ROCK!
-You Are Super Awesome!
+You Are Super Awesome!:I can't deny it
+You can do it!
+The possibilities are endless!:I'm going to seize a few!
